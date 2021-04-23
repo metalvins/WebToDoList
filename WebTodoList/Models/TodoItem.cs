01b975ba26d254daa6b118b94864974ea7b2355a
Vinesh Thakur <vineshthakur@gmail.com>
2021-04-24 04:51:49 +0530

    * TodoItem.cs:
    * WebTodoList.csproj: Created a ToDoItem Model class

---
+++ WebTodoList/Models/TodoItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebTodoList.Models
+{
+    public class TodoItem
+    {
+		[Key]
+		public int ItemId { get; set; }
+
+		[DataType(DataType.EmailAddress)]
+		public String Email { get; set; }
+
+		[MaxLength(100)]
+        public String Title { get; set; }
+
+		[MaxLength(300)]
+        public String Description { get; set; }
+
+		[Display(Name = "Date Added")]
+		public DateTime AddedDate { get; set; } = DateTime.Now;
+
+		[Display(Name = "Due Date")]
+		public DateTime DueDate { get; set; } = DateTime.Now;
+
+		[Display(Name = "Date Completed")]
+		public DateTime DoneDate { get; set; } = DateTime.Now;
+
+		public Boolean Done { get; set; } = false;
+	}
+}
